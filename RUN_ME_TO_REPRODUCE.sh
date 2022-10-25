1a2590440b183454149515c43a6f4fcdcdb700ff
Mark Philpot <mark.philpot@gmail.com>
2022-10-24 21:05:30 -0700

    Can't reproduce via command line

--- RUN_ME_TO_REPRODUCE.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir tmp/my_post.textbundle
-cp content/blog/my_post.textbundle/info.json tmp/my_post.textbundle
-cp content/blog/my_post.textbundle/text.md tmp/my_post.textbundle
-echo "SOME UPDATES" >> tmp/my_post.textbundle/text.md
-rm -rf content/blog/my_post.textbundle
-mv tmp/my_post.textbundle content/blog/.
